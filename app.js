437780a763ba7cccf8df0ffc1058004d2c6541aa
Taslim J Dosunmu <jaydosunmu@Taslims-MacBook-Pro.local>
2017-10-15 02:54:40 -0400

    open default convo

--- app.js
+++ app.js
@@ -33,25 +33,25 @@ var bot = new builder.UniversalBot(connector);
 
 
 
-// bot.dialog('/', [
-//     function (session) {
-//         builder.Prompts.text(session, "Hello... What's your name?");
-//     },
-//     function (session, results) {
-//         session.userData.name = results.response;
-//         builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
-//     },
-//     function (session, results) {
-//         session.userData.coding = results.response;
-//         builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-//     },
-//     function (session, results) {
-//         session.userData.language = results.response.entity;
-//         session.send("Got it... " + session.userData.name + 
-//                     " you've been programming for " + session.userData.coding + 
-//                     " years and use " + session.userData.language + ".");
-//     }
-// ]);
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name + 
+                    " you've been programming for " + session.userData.coding + 
+                    " years and use " + session.userData.language + ".");
+    }
+]);
 
 var luisAppId = process.env.LuisAppId;
 var luisAPIKey = process.env.LuisAPIKey;
